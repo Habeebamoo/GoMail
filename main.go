d8c3b27bf6c50d8f2642743e69e047b9cfa5881f
Habeebamoo <habeebamoo08@gmail.com>
2025-05-18 15:30:36 +0100

    implemented config file

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"net/smtp"
 	"os"
 )
+
+type Config struct {
+	Sender string `json:"sender"`
+	Password string `json:"password"`
+}
 
 func readMessage() ([]byte, error) {
 	msg, err := os.ReadFile("res/message.txt")
@@ -17,11 +24,46 @@ func readMessage() ([]byte, error) {
 
 			_, err = os.Create("res/message.txt")
 			check(err)
+		} else {
+			return nil, errors.New("can't read res/message.txt file")
 		}
-		return nil, errors.New("can't read res/message.txt file")
 	}
 
 	return msg, nil
+}
+
+func saveCred(sender, password string) error {
+	config := Config{sender, password}
+	configJson, err := json.MarshalIndent(config, "", " ")
+	check(err)
+
+	err = os.WriteFile("config.json", configJson, 0644)
+	if err != nil {
+		if os.IsNotExist(err) {
+			_, err = os.Create("config.json")
+			check(err)
+		} else {
+			return errors.New("can't read config.json file")
+		}
+	}
+
+	fmt.Println("Your credentials have been saved")
+	return nil
+}
+
+func getCred() (Config, error) {
+	configJson, err := os.ReadFile("config.json")
+	if err != nil {
+		return Config{}, errors.New("can't read config file")
+	}
+
+	var data Config
+	err = json.Unmarshal(configJson, &data)
+	if err != nil {
+		return Config{}, errors.New("can't format config file")
+	}
+
+	return data, nil
 }
 
 func check(err error) {
@@ -32,24 +74,55 @@ func check(err error) {
 }
 
 func main() {
-	from := "habeebamoo08@gmail.com"
-	password := "exphqvkpdzrbhrdp"
+	var action string
+	var sender string
+	var pass string
+	var receiver string
 
-	to := []string{"alexjohnson99.uk@gmail.com"}
+	flag.StringVar(&action, "a", "", "save or send")
+	flag.StringVar(&sender, "from", "", "Senders Gmail's Address")
+	flag.StringVar(&pass, "password", "", "Senders Gmail's App Password")
+	flag.StringVar(&receiver, "to", "", "Receiver's Email Address")
 
-	smtpHost := "smtp.gmail.com"
-	smtpPort := "587"
+	flag.Parse()
 
-	auth := smtp.PlainAuth("", from, password, smtpHost)
+	switch action {
+	case "save":
+		if action == "" || sender == "" || pass == "" {
+			fmt.Println("You must provide action, sender and password")
+			return
+		}
 
-	body, err := readMessage()
-	check(err)
-	msg := []byte("Subject: GoMail\r\n\r\n" + string(body))
+		err := saveCred(sender, pass)
+		check(err)
+	case "send":
+		if action == "" || receiver == "" {
+			fmt.Println("Define you action -a send")
+			return
+		}
 
-	err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, msg)
-	if err != nil {
-		log.Fatal(err)
+		data, err := getCred()
+		check(err)
+		
+		from := data.Sender
+		password := data.Password
+
+		to := []string{receiver}
+
+		smtpHost := "smtp.gmail.com"
+		smtpPort := "587"
+
+		auth := smtp.PlainAuth("", from, password, smtpHost)
+
+		body, err := readMessage()
+		check(err)
+		msg := []byte("Subject: GoMail\r\n\r\n" + string(body))
+
+		err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Email sent successfully")
 	}
-
-	fmt.Println("Email sent successfully")
 }
