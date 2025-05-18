ae27f0e3a1b069a176c7c7ee4dd8e261b3af133e
Habeebamoo <habeebamoo08@gmail.com>
2025-05-18 15:07:23 +0100

    added message file

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/smtp"
+	"os"
 )
+
+func readMessage() ([]byte, error) {
+	msg, err := os.ReadFile("res/message.txt")
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.Mkdir("res", 0755)
+			check(err)
+
+			_, err = os.Create("res/message.txt")
+			check(err)
+		}
+		return nil, errors.New("can't read res/message.txt file")
+	}
+
+	return msg, nil
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
 
 func main() {
 	from := "habeebamoo08@gmail.com"
@@ -17,9 +42,11 @@ func main() {
 
 	auth := smtp.PlainAuth("", from, password, smtpHost)
 
-	msg := []byte("Subject: GoMail")
+	body, err := readMessage()
+	check(err)
+	msg := []byte("Subject: GoMail\r\n\r\n" + string(body))
 
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, msg)
+	err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, msg)
 	if err != nil {
 		log.Fatal(err)
 	}
